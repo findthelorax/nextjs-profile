3137bf565061473a1936d452a3f9ca3f1296156a
Brett Ferrante <findthelorax@gmail.com>
2024-09-20 14:49:05 -0400

    react hook 9

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -21,38 +21,40 @@ const useProjectAnimations = (index: number) => {
 };
 
 const Projects = () => {
+    const projectElements = projects.map((project, index) => {
+        const { title, description, languages, image, url } = project;
+        const { ref, props } = useProjectAnimations(index);
+        const alignment = index % 2 === 0 ? 'justify-start' : 'justify-end';
+
+        return (
+            <animated.div
+                ref={ref}
+                style={props}
+                className={`flex ${alignment} items-center`}
+                key={index}
+            >
+                {index % 2 === 0 ? (
+                    <>
+                        <ProjectImage image={image} title={title} url={url} />
+                        <ProjectCard title={title} description={description} languages={languages} />
+                    </>
+                ) : (
+                    <>
+                        <ProjectCard title={title} description={description} languages={languages} />
+                        <ProjectImage image={image} title={title} url={url} />
+                    </>
+                )}
+            </animated.div>
+        );
+    });
+
     return (
         <section id="projects" className='container mx-auto pt-20' style={{ overflowX: 'hidden' }}>
             <div className="mx-auto">
                 <div className="w-full text-white text-center py-2 projects-title">
                     <h1>PROJECTS</h1>
                 </div>
-                {projects.map((project, index) => {
-                    const { title, description, languages, image, url } = project;
-                    const { ref, props } = useProjectAnimations(index);
-                    const alignment = index % 2 === 0 ? 'justify-start' : 'justify-end';
-
-                    return (
-                        <animated.div
-                            ref={ref}
-                            style={props}
-                            className={`flex ${alignment} items-center`}
-                            key={index}
-                        >
-                            {index % 2 === 0 ? (
-                                <>
-                                    <ProjectImage image={image} title={title} url={url} />
-                                    <ProjectCard title={title} description={description} languages={languages} />
-                                </>
-                            ) : (
-                                <>
-                                    <ProjectCard title={title} description={description} languages={languages} />
-                                    <ProjectImage image={image} title={title} url={url} />
-                                </>
-                            )}
-                        </animated.div>
-                    );
-                })}
+                {projectElements}
             </div>
         </section>
     );
